bca5f2934c10253d27e685d55d175f772a4245b3
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-09-11 15:35:22 +0530

    creted form structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ function App() {
 
   return (
     <>
-      hi
+      <div className='container max-w-3xl mx-auto p-6 mt-10 bg-gray-50 rounded-lg shadow-lg'>
+        <h1 className='text-2xl font-bold text-center mb-4'>URL Hashing and Shortner</h1>
+        <form action="" method="post" className='space-y-4'>
+          <div>
+            <label className='block text-sm font-medium text-gray-700'>Enter URL</label>
+            <input type="text" placeholder='Enter the URL' required className='block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-300' />
+          </div>
+          <div>
+            <label className='block text-sm font-medium text-gray-700'>Max Access (optional)</label>
+            <input type="text" placeholder='Enter Max Access Number (Optional)' className='block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-300' />
+          </div>
+          <div>
+            <button type='submit' className='w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300'>Submit</button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
